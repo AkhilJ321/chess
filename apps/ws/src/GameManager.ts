1d6bfb151b8fd57fbf5b7c865ff3dfb60a1bc6d2
AkhilJ321 <jamwalakhil936@gmail.com>
2024-06-19 23:59:27 +0530

    shifted to monorepo

---
+++ apps/ws/src/GameManager.ts
@@ -0,0 +1,59 @@
+import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./messages";
+import { WebSocket } from "ws";
+import { db } from "./db";
+
+export class GameManager {
+  private games: Game[];
+  private pendingUser: WebSocket | null;
+  private users: WebSocket[];
+  public getInstance() {}
+
+  constructor() {
+    this.games = [];
+    this.pendingUser = null;
+    this.users = [];
+  }
+  addUser(socket: WebSocket) {
+    this.users.push(socket);
+    this.addHandler(socket);
+  }
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter((user) => user !== socket);
+    // stop the game here as user left
+  }
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (data) => {
+      const message = JSON.parse(data.toString());
+
+      if (message.type === INIT_GAME) {
+        if (this.pendingUser) {
+          // start a game
+          const game = new Game(this.pendingUser, socket);
+          this.games.push(game);
+          // store an entry in the database
+          db.game.create({
+            data: {
+              player1: "idtobeadded",
+              player2: "someId",
+              winner: null,
+            },
+          });
+
+          this.pendingUser = null;
+        } else {
+          this.pendingUser = socket;
+        }
+      }
+
+      if (message.type === MOVE) {
+        const game = this.games.find(
+          (game) => game.player1 === socket || game.player2 === socket
+        );
+        if (game) {
+          game.makeMove(socket, message.payload.move);
+        }
+      }
+    });
+  }
+}
